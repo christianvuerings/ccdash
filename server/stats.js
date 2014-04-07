8f50e648688a0074393a667b7e098701cc5a69a5
Chris Tweney <chris@media.berkeley.edu>
2014-04-07 14:11:13 -0700

    Use span instead of a, and add var where needed

--- server/stats.js
+++ server/stats.js
@@ -10,7 +10,7 @@ var parseStats = function(stats) {
 
   // hot plate
   stats.hot_plate.average_time = (stats.hot_plate.total_warmup_time / stats.hot_plate.total_warmups_processed).toFixed(1);
-  outstanding = stats.hot_plate.total_warmups_requested - stats.hot_plate.total_warmups_processed;
+  var outstanding = stats.hot_plate.total_warmups_requested - stats.hot_plate.total_warmups_processed;
   if (outstanding) {
     stats.hot_plate.outstanding_warmups = outstanding;
     stats.hot_plate.state = "ccdash-state-warning";
@@ -19,7 +19,7 @@ var parseStats = function(stats) {
   }
 
   // live updates
-  averageTime = (stats.live_updates_warmer.total_warmup_time / stats.live_updates_warmer.total_warmups_requested).toFixed(1);
+  var averageTime = (stats.live_updates_warmer.total_warmup_time / stats.live_updates_warmer.total_warmups_requested).toFixed(1);
   stats.live_updates_warmer.average_time = averageTime;
   if (averageTime > 5) {
     stats.live_updates_warmer.state = "ccdash-state-warning";
